d93591eba67ea6776cc1a5be139c9ba62ba8ad41
DannyPY <olatundedaniel943@gmail.com>
2025-01-06 23:27:33 -0800

    my first node app

---
+++ app.js
@@ -0,0 +1,11 @@
+const amount = 12;
+
+if (amount < 12){
+    console.log('This is a small number thsn 12');
+} else {
+    console.log('This a lager number than 12')
+};
+
+console.log('Hello world!, This is my first node application');
+
+

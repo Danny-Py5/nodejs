29c3d2d4b58da2e960c853bd0efcc73bd1169d7c
DannyPY <olatundedaniel943@gmail.com>
2025-01-24 12:50:50 -0800

    custom error  instance specification

--- 03-task-manager/starter/milddleware/error-handler.js
+++ 03-task-manager/starter/milddleware/error-handler.js
@@ -1,10 +1,15 @@
+const { CustomAPIError } = require('../error/customError.js');
+
 /**
  * @param {import('express').Request} req - The request object
  * @param {import('express').Response} res - The response object
  */
 const errorHandlerMiddleware = (err, req, res, next) => {
     //console.log(err);
-    return res.status(err.statusCode).json({msg: err.message, status: err.statusCode, success: false});
+    if (err instanceof CustomAPIError){
+        return res.status(err.statusCode).json({msg: err.message, status: err.statusCode, success: false});
+    }
+    res.status(500).send('Something went wrong, try again later.');
 };
 
 
